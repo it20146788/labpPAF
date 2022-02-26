4f766cc912d7b1d3d81108554fde4750f18b0c9a
it20146788 <it20146788@my.sliit.lk>
2022-02-26 17:43:41 +0530

    commit4

--- paflab1/src/paflab1/temp.java
+++ paflab1/src/paflab1/temp.java
@@ -5,7 +5,7 @@ public class temp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("jhjj bell");
-		System.out.println("jhjj be4ll");
+		System.out.println("jhjj be554ll");
 		
 	}
 
